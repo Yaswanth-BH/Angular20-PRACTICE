985728ac0b2b2d1bcc2705cff6f975cfbc02aeaa
Yaswanth-BH <bharanikapulayaswanthofficial@gmail.com>
2025-11-17 12:53:30 +0530

    Episode-6: Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { User } from './components/user/user';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'admin', component:Admin },
+    { path:'user', component:User },
+    { path:'control-flow-statements', component:ControlFlow },
+    { path:'data-binding', component:DataBinding },
+    { path:'signal', component:SignalEx },
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Admin } from './components/admin/admin';
-import { User } from './components/user/user';
-import { DataBinding } from './components/data-binding/data-binding';
-import { SignalEx } from './components/signal-ex/signal-ex';
-import { ControlFlow } from './components/control-flow/control-flow';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Admin,User,DataBinding,SignalEx,ControlFlow],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
