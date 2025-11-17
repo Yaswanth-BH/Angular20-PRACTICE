e11acaf31808b8e63e261dda8de97d9af935b6ae
Yaswanth-BH <bharanikapulayaswanthofficial@gmail.com>
2025-11-17 14:59:29 +0530

    Episode-7: Directives

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { User } from './components/user/user';
 import { ControlFlow } from './components/control-flow/control-flow';
 import { DataBinding } from './components/data-binding/data-binding';
 import { SignalEx } from './components/signal-ex/signal-ex';
+import { AttDirective } from './components/att-directive/att-directive';
 
 export const routes: Routes = [
-    { path:'admin', component:Admin },
-    { path:'user', component:User },
-    { path:'control-flow-statements', component:ControlFlow },
-    { path:'data-binding', component:DataBinding },
-    { path:'signal', component:SignalEx },
+    { path: 'admin', component: Admin },
+    { path: 'user', component: User },
+    { path: 'control-flow-statements', component: ControlFlow },
+    { path: 'data-binding', component: DataBinding },
+    { path: 'signal', component: SignalEx },
+    { path: 'attribute-directive', component: AttDirective },
 
 ];
